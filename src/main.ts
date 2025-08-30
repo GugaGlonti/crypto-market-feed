a5d352cbbd07fb75d7196347dc8dc01f963355aa
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2025-08-30 23:48:44 +0200

    feat: adjustable loglevel

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 void (async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: [process.env.NODE_ENV === 'production' ? 'error' : 'debug'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 })();
